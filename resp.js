9681747cede1977cc5558c06deb270a8b0d5a65b
cndiscounts <132609819+cndiscounts@users.noreply.github.com>
2023-05-05 00:23:41 +0530

    files

---
+++ resp.js
@@ -0,0 +1,32 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navList = document.querySelector('.nav-list')
+rightNav = document.querySelector('.rightNav')
+ 
+
+
+burger.addEventListener('click', ()=>{
+    rightNav.classList.toggle('v-class-resp');
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+    
+})
+
+function myFunction() {
+    // Get the text field
+    var copyText = document.getElementById("myInput");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+  }
+
+function redirecttopage(){
+    
+}
